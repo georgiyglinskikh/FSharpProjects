ad5aff7092869e3f7707234b36cee1f12601881f
georgiyglinskikh <georgiyglinskikh@gmail.com>
2020-01-15 21:48:04 +0500

    Format Code

--- Calculator/Program.fs
+++ Calculator/Program.fs
@@ -1,13 +1,16 @@
-﻿open System
+open System
 open System.Text.RegularExpressions
 open System.Collections.Generic
 
 let value operator =
     match operator with
-    | "+" | "-" -> 1
-    | "*" | "/" -> 2
+    | "+"
+    | "-" -> 1
+    | "*"
+    | "/" -> 2
     | "**" -> 3
-    | "(" | ")" -> -1
+    | "("
+    | ")" -> -1
     | _ -> 0
 
 let perform opertator num0 num1 =
@@ -20,31 +23,38 @@ let perform opertator num0 num1 =
     | _ -> 0
 
 let calculate expression =
-    let stuff = Regex.Matches(expression, @"(\d+)|(\+|\-|\*|\/|(\*\*))") |> ( fun col -> [ for i in col -> i.Value] )
+    let stuff =
+        Regex.Matches(expression, @"(\d+)|(\+|\-|\*|\/|(\*\*))")
+        |> (fun col -> [ for i in col -> i.Value ])
+
     let allNumbers = Queue<int>()
-    stuff |> List.filter (fun el -> value el = 0) |> List.map int |> List.iter allNumbers.Enqueue 
+    stuff
+    |> List.filter (fun el -> value el = 0)
+    |> List.map int
+    |> List.iter allNumbers.Enqueue
     let allOperators = Queue<string>()
-    stuff |> List.filter (fun el -> value el <> 0) |> List.iter allOperators.Enqueue
+    stuff
+    |> List.filter (fun el -> value el <> 0)
+    |> List.iter allOperators.Enqueue
 
     let currentNum = Stack()
     let currentOp = Stack()
 
-    currentNum.Push (allNumbers.Dequeue())
-    currentOp.Push (allOperators.Dequeue())
-    currentNum.Push (allNumbers.Dequeue())
+    currentNum.Push(allNumbers.Dequeue())
+    currentOp.Push(allOperators.Dequeue())
+    currentNum.Push(allNumbers.Dequeue())
 
     while currentOp.Count > 0 do
         let op1 = currentOp.Pop()
         if allOperators.Count > 0 then
             let op0 = allOperators.Dequeue()
-            if value op0 < value op1 then
-                currentNum.Push (perform op1 (currentNum.Pop()) (currentNum.Pop()))
-            else
-                currentOp.Push(op1)
+            if value op0 < value op1
+            then currentNum.Push(perform op1 (currentNum.Pop()) (currentNum.Pop()))
+            else currentOp.Push(op1)
             currentNum.Push(allNumbers.Dequeue())
             currentOp.Push(op0)
         else
-            currentNum.Push (perform op1 (currentNum.Pop()) (currentNum.Pop()))
+            currentNum.Push(perform op1 (currentNum.Pop()) (currentNum.Pop()))
 
     currentNum.Pop()
 
@@ -54,4 +64,4 @@ let main argv =
     Console.ReadLine()
     |> calculate
     |> Console.WriteLine
-    0 
+    0
--- TextGUI/Program.fs
+++ TextGUI/Program.fs
@@ -14,24 +14,30 @@ let RegEx reg str =
     let m = Regex.Matches(str, reg)
     [| for i in m -> i.Groups.[1].Value |]
 
-let get path regex = path |> http |> RegEx regex
+let get path regex =
+    path
+    |> http
+    |> RegEx regex
 
 let getReferences path = get path @"<A HREF=""([^""]*)"">"
 let getText path = get path @"(\w+)"
 
-let getAuthors () = getReferences url
-let getBooks author = getReferences (url + author + "/") 
+let getAuthors() = getReferences url
+let getBooks author = getReferences (url + author + "/")
 let getUrlBooksText author book = (url + author + "/" + book)
+
 
 [<EntryPoint>]
 let main argv =
     Application.EnableVisualStyles()
     Application.SetCompatibleTextRenderingDefault false
 
-    let form = new Form(Size = Size(800, 600), Text="Частота слов / Word frequency", FormBorderStyle = FormBorderStyle.FixedDialog)
+    let form =
+        new Form(Size = Size(800, 600), Text = "Частота слов / Word frequency",
+                 FormBorderStyle = FormBorderStyle.FixedDialog)
 
     let authorBox = new ComboBox(Location = Point(10, 10), Size = Size(100, 23))
-    authorBox.Items.AddRange(Array.map (fun (a: string) -> box(string a.[0] + a.[1..].ToLower())) (getAuthors()))
+    authorBox.Items.AddRange(Array.map (fun (a: string) -> box (string a.[0] + a.[1..].ToLower())) (getAuthors()))
 
     let bookBox = new ComboBox(Location = Point(120, 10), Size = Size(500, 23))
     bookBox.Enabled <- false
@@ -40,38 +46,51 @@ let main argv =
     getTextButton.Enabled <- false
 
     // Text box position = 0 + 10 + 23 + 10 = 43
-    let textBox = new TextBox(Location = Point(10, 43), Size = Size(760, 500), ReadOnly = true, Multiline = true, ScrollBars = ScrollBars.Vertical)
+    let textBox =
+        new TextBox(Location = Point(10, 43), Size = Size(760, 500), ReadOnly = true, Multiline = true,
+                    ScrollBars = ScrollBars.Vertical)
     textBox.Enabled <- false
 
-    getTextButton.Click.Add(fun args -> 
+    getTextButton.Click.Add(fun args ->
         if authorBox.Items.Contains(authorBox.Text) && bookBox.Items.Contains(bookBox.Text) then
             textBox.Enabled <- true
-            let wordsByDecending = (getUrlBooksText (authorBox.Text.ToUpper()) bookBox.Text) |> getText |> Array.map (fun el -> el.ToLower()) |> Array.groupBy id |> Array.sortByDescending (fun el -> (snd el).Length) |> Array.map (fun a -> ((fst a), (snd a).Length))
-            textBox.Text <- Array.fold (fun a b -> a + (b |> fst |> string) + " - " + (b |> snd |> string) + "\r\n") " " wordsByDecending
-        else 
+            let wordsByDecending =
+                (getUrlBooksText (authorBox.Text.ToUpper()) bookBox.Text)
+                |> getText
+                |> Array.map (fun el -> el.ToLower())
+                |> Array.groupBy id
+                |> Array.sortByDescending (fun el -> (snd el).Length)
+                |> Array.map (fun a -> ((fst a), (snd a).Length))
+            textBox.Text <-
+                Array.fold (fun a b ->
+                    a + (b
+                         |> fst
+                         |> string) + " - " + (b
+                                               |> snd
+                                               |> string)
+                    + "\r\n") " " wordsByDecending
+        else
             textBox.Enabled <- false
-            textBox.Text <- ""
-    )
+            textBox.Text <- "")
 
-    authorBox.SelectedValueChanged.Add(fun args -> 
+    authorBox.SelectedValueChanged.Add(fun args ->
         if authorBox.Items.Contains authorBox.Text then
             bookBox.Enabled <- true
-            bookBox.Items.AddRange(Array.map (fun (a: string) -> box(a)) (getBooks(authorBox.Text.ToUpper())))
-        else 
+            bookBox.Items.AddRange(Array.map (fun (a: string) -> box (a)) (getBooks (authorBox.Text.ToUpper())))
+        else
             bookBox.Enabled <- false
             getTextButton.Enabled <- false
-            for i in 0..(bookBox.Items.Count - 1) do
-                bookBox.Items.RemoveAt(i)
-    )
-    
-    bookBox.SelectedValueChanged.Add(fun args -> 
-        if bookBox.Items.Contains bookBox.Text then
-            getTextButton.Enabled <- true
-        else
-            getTextButton.Enabled <- false
-    )
+            for i in 0 .. (bookBox.Items.Count - 1) do
+                bookBox.Items.RemoveAt(i))
 
-    form.Controls.AddRange([|authorBox; bookBox; getTextButton; textBox|])
+    bookBox.SelectedValueChanged.Add
+        (fun args ->
+            if bookBox.Items.Contains bookBox.Text then
+                getTextButton.Enabled <- true
+            else
+                getTextButton.Enabled <- false)
+
+    form.Controls.AddRange([| authorBox; bookBox; getTextButton; textBox |])
 
     Application.Run form
     0
